a8ca04275f755e8349b707ae73a873a0fa00f1ea
BrendonPierson <brendonpierson@gmail.com>
2015-08-04 10:51:37 -0500

    added box shadow

--- javascripts/main.js
+++ javascripts/main.js
@@ -11,10 +11,10 @@ requirejs(
         songs[songs.length] = data[i];
       }
       for (i = 0; i < songs.length; i++) {
-        infoHTML +=   "<div class='song'><h3>" + songs[i].name + "</h3>" + 
-                      "<p>Artist: " + songs[i].artist + "</p>" +
-                      "<p>On the album: " + songs[i].album + "</p>" + 
-                      '<button type="button" value="delete">Delete Song</button></div>';
+        infoHTML +=   "<div class='row song'><h3>" + songs[i].name + "</h3>" + 
+                      "<div class='row'><p class='col-sm-4'>Artist: " + songs[i].artist + "</p>" +
+                      "<p class='col-sm-4'>On the album: " + songs[i].album + "</p>" + 
+                      '<div class="col-sm-4"><button type="button" value="delete" class="btn btn-danger btn-xs">Delete Song</button></div></div></div>';
       } 
       dom.songsDiv.prepend(infoHTML);
     }
@@ -28,7 +28,7 @@ requirejs(
 
     $("#info").on('click', "button[value='delete']", function(){
       console.log("clicked delete");
-      $(this).parent().remove();
+      $(this).parent().parent().parent().remove();
     });
 
   }
