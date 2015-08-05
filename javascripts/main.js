98802cbb09171c48c2265ff68940d92354d706bd
BrendonPierson <brendonpierson@gmail.com>
2015-08-05 14:17:17 -0500

    added fixed controls

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,7 +6,7 @@ requirejs.config({
     'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
   },
   shim: {
-    'bootstrap': ['jquery']
+    'bootstrap': ['jquery'] //makes sure jquery is loaded before bootstrap
   }
 });
 
@@ -14,21 +14,42 @@ requirejs(
   ["jquery", "hbs", "bootstrap", "dom-access", "populate-songs", "get-more-songs"], 
   function($, Handlebars, bootstrap, dom, songs1, songs2) {
 
+    //function for turning json file into html
     function putSongsInHTML(data) {
       require(['hbs!../templates/songs'],function(songTemplate){
         dom.songsDiv.hide("fast").prepend(songTemplate(data)).slideDown("slow");
       });
     }
-
+    //populate songs list
     songs1.getSongs(putSongsInHTML);
 
+    //more sounds button click event handler, hides button when songs are added
     dom.moreSongs.click(function(){
       songs2.getSongs(putSongsInHTML);
+      $(this).hide();
     });
 
+    //handle delete button click
     $(dom.songsDiv).on('click', "button[value='delete']", function(){
       console.log("clicked delete");
       $(this).parent().parent().parent().remove();
     });
-  }
-);
+
+    //Fix controls when window is > 767
+    if($(window).width() > 767){
+      $("#controls").addClass("affix");
+      $("#info").removeClass("col-sm-offset-1").addClass("col-sm-offset-4");
+    }
+
+    $( window ).resize(function(){
+      if($(window).width() > 767){
+        $("#controls").addClass("affix");
+        $("#info").removeClass("col-sm-offset-1").addClass("col-sm-offset-4");
+      } else if ($(window).width() < 767){
+        $("#controls").removeClass("affix");
+        $("#info").removeClass("col-sm-offset-4").addClass("col-sm-offset-1");
+      }
+    });
+    
+  } // end of require js function
+); // end of require js scope
