73f4d6a093727716025fafbe15146ccd863ec308
BrendonPierson <brendonpierson@gmail.com>
2015-08-05 10:33:13 -0500

    added slide down animation

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,9 @@ requirejs(
   function(dom, songs1, songs2) {
 
     var songs = [];
-    var extraSongs = [];
     var infoHTML = '';
+    // var extraSongs = [];
+    // var moreInfoHTML = '<div id="moreSongs" class="hidden">';
 
     function putSongsInHTML(data) {
       for(var i = 0; i < data.length; i++) {
@@ -16,20 +17,40 @@ requirejs(
                       "<p class='col-sm-4'>On the album: " + songs[i].album + "</p>" + 
                       '<div class="col-sm-4"><button type="button" value="delete" class="btn btn-danger btn-sm">Delete Song</button></div></div></div>';
       } 
-      dom.songsDiv.prepend(infoHTML);
+      dom.songsDiv.hide("fast").prepend(infoHTML).slideDown("slow");
     }
+
+    // function putMoreSongsInHTML(data) {
+    //   for(var i = 0; i < data.length; i ++) {
+    //     extraSongs[extraSongs.length] = data[i];
+    //   }
+    //   for (i = 0; i < extraSongs.length; i++){
+    //     moreInfoHTML +=   "<div class='row song'><h3>" + songs[i].name + "</h3>" + 
+    //                       "<div class='row'><p class='col-sm-4'>Artist: " + songs[i].artist + "</p>" +
+    //                       "<p class='col-sm-4'>On the album: " + songs[i].album + "</p>" + 
+    //                       '<div class="col-sm-4"><button type="button" value="delete" class="btn btn-danger btn-sm">Delete Song</button></div></div></div>';
+    //   }
+    //   moreInfoHTML += '</div>';
+    //   dom.songsDiv.prepend(moreInfoHTML);
+    //   $("#moreSongs").slideDown("slow");
+    // }
+
     songs1.getSongs(putSongsInHTML);
+
 
     $("#addMoreSongs").click(function(){
       infoHTML = '';
       songs2.getSongs(putSongsInHTML);
-       $("#info").html(infoHTML);
+      $(dom.songsDiv).html(infoHTML);
     });
 
-    $("#info").on('click', "button[value='delete']", function(){
+    $(dom.songsDiv).on('click', "button[value='delete']", function(){
       console.log("clicked delete");
       $(this).parent().parent().parent().remove();
     });
+
+
+
   }
 
 );
